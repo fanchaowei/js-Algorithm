6655cf58f09ae14470acf1da2c2825affa8d5e50
fanchaowei <597064177@qq.com>
2022-08-24 22:27:25 +0800

    增加解法

--- leetcode/509.斐波那契数.js
+++ leetcode/509.斐波那契数.js
@@ -9,11 +9,34 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function (n) {
-  let dp = [0, 1]
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2]
+// var fib = function (n) {
+//   // 第一种：递归
+//   // if(n<=1) {
+//   //   return n
+//   // }
+//   // return fib(n-1) + fib(n-2)
+//   // ------
+//   // 第二种：动态规划
+//   // let dp = [0, 1]
+//   // for (let i = 2; i <= n; i++) {
+//   //   dp[i] = dp[i - 1] + dp[i - 2]
+//   // }
+//   // return dp[n]
+// }
+
+// 第三种 递归+备忘录
+function helper(memo, n) {
+  if (n <= 1) {
+    return n
   }
-  return dp[n]
+  if (memo[n]) {
+    return memo[n]
+  }
+  memo[n] = helper(memo, n - 1) + helper(memo, n - 2)
+  return memo[n]
+}
+var fib = function (n) {
+  let memo = []
+  return helper(memo, n)
 }
 // @lc code=end
