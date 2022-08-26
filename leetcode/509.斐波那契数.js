b606600923ed2e388eef07491d273f1fa3bd3954
fanchaowei <597064177@qq.com>
2022-08-26 21:52:42 +0800

    增加 509 解题的方式

--- leetcode/509.斐波那契数.js
+++ leetcode/509.斐波那契数.js
@@ -25,18 +25,41 @@
 // }
 
 // 第三种 递归+备忘录
-function helper(memo, n) {
+// 好处是避免一些重复的相加
+// function helper(memo, n) {
+//   if (n <= 1) {
+//     // 递归的结束条件
+//     return n
+//   }
+//   if (memo[n]) {
+//     // 递归过程中，如果发现 memo 中已经存在该值，直接返回，减少了一次重复的计算
+//     return memo[n]
+//   }
+//   // 递归去相加，递归过程中会出现重复计算的，但我们存在备忘录 memo，在递归的过程中直接返回
+//   memo[n] = helper(memo, n - 1) + helper(memo, n - 2)
+//   return memo[n]
+// }
+// var fib = function (n) {
+//   // 记录所有计算后得出的斐波那契数
+//   let memo = []
+//   return helper(memo, n)
+// }
+
+// 第四种 动态规划 减少空间复杂度
+var fib = function (n) {
   if (n <= 1) {
     return n
   }
-  if (memo[n]) {
-    return memo[n]
+  // 不使用数组而使用变量去记录，减少空间复杂度
+  let dp1 = 0
+  let dp2 = 1
+  let dp3
+  for (let i = 2; i <= n; i++) {
+    dp3 = dp1 + dp2
+    dp1 = dp2
+    dp2 = dp3
   }
-  memo[n] = helper(memo, n - 1) + helper(memo, n - 2)
-  return memo[n]
+  return dp3
 }
-var fib = function (n) {
-  let memo = []
-  return helper(memo, n)
-}
+
 // @lc code=end
