fd0a641a13ad0265d04832710a863918a39dbd4d
fanchaowei <597064177@qq.com>
2023-08-02 23:09:28 +0800

    review: 重答 15

--- leetcode/15.三数之和.js
+++ leetcode/15.三数之和.js
@@ -5,26 +5,70 @@
  */
 
 // @lc code=start
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
+// 实现该算法的逻辑：
+//   left->  <-right
+// [-1,0,1,2,-1,-4]
+// 先将数组进行排序
+// 循环依次将最前位 i 拿出，然后在取出后的数组设立左右指针。
+// left 从 i+1 开始依次往右走
+// right 从数组最后一位开始往左走
+// 当 i、left、right 的数组值相加为 0 ，则找到。
+// 如果值偏小则是 left 值太小，往右走一位。偏大则 right 往左走一位
+// var threeSum = function (nums) {
+//   if (nums.length < 3) {
+//     return []
+//   }
+//   const list = []
+
+//   // 对数组进行排序
+//   nums.sort((a, b) => a - b)
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === nums[i - 1]) {
+//       continue
+//     }
+//     let left = i + 1
+//     let right = nums.length - 1
+//     while (left < right) {
+//       const count = nums[i] + nums[left] + nums[right]
+//       if (right === i) {
+//         right--
+//       } else if (count === 0) {
+//         // 将找到的值存入
+//         list.push([nums[i], nums[left], nums[right]])
+//         // 存入后继续寻找，判断一下左右的值和剩余内部的值，如果是相等的则跳过，因为等到的结果会和上面存入的是一样的。
+//         while (nums[left] === nums[left + 1]) {
+//           left++
+//         }
+//         left++
+//         while (nums[right] === nums[right - 1]) {
+//           right--
+//         }
+//         right--
+//       } else if (count > 0) {
+//         // 值大于0，说明右边的值过大。
+//         right--
+//       } else {
+//         // 值小于0，说明左边的值太小了
+//         left++
+//       }
+//     }
+//   }
+//   return list
+// }
+
 var threeSum = function (nums) {
-  // 实现该算法的逻辑：
-  //   left->  <-right
-  // [-1,0,1,2,-1,-4]
-  // 循环以此次将最前位 i 拿出，然后在取出后的数组设立左右指针。
-  // left 从 i+1 开始依次往右走
-  // right 从数组最后一位开始往左走
-  // 当 i、left、right 的数组值相加为 0 ，则找到。
-  // 如果值偏小则是 left 值太小，往右走一位。偏大则 right 往左走一位
   if (nums.length < 3) {
     return []
   }
-  const list = []
 
-  // 对数组进行排序
+  const list = []
   nums.sort((a, b) => a - b)
+  // console.log('nums', nums)
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === nums[i - 1]) {
       continue
@@ -32,13 +76,10 @@ var threeSum = function (nums) {
     let left = i + 1
     let right = nums.length - 1
     while (left < right) {
-      const count = nums[i] + nums[left] + nums[right]
-      if (right === i) {
-        right--
-      } else if (count === 0) {
-        // 将找到的值存入
+      let sum = nums[i] + nums[left] + nums[right]
+      // console.log(`第${i + 1}次`, sum, i, left, right)
+      if (sum === 0) {
         list.push([nums[i], nums[left], nums[right]])
-        // 存入后继续寻找，判断一下左右的值和剩余内部的值，如果是相等的则跳过，因为等到的结果会和上面存入的是一样的。
         while (nums[left] === nums[left + 1]) {
           left++
         }
@@ -47,15 +88,15 @@ var threeSum = function (nums) {
           right--
         }
         right--
-      } else if (count > 0) {
-        // 值大于0，说明右边的值过大。
+      } else if (sum > 0) {
         right--
-      } else {
-        // 值小于0，说明左边的值太小了
+      } else if (sum < 0) {
         left++
       }
     }
   }
   return list
 }
+// console.log(threeSum([1, -1, -1, 0]))
+
 // @lc code=end
