110a55cef771ac1f71401a084da862753b24a69e
HunterCat33 <huntercat333@gmail.com>
2025-12-22 17:02:42 +0800

    feat: 234题（暂未完成）

--- leetcode/234.回文链表.js
+++ leetcode/234.回文链表.js
@@ -16,41 +16,91 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+
 var isPalindrome = function (head) {
-  // 本题先通过快慢指针，将数组前部分颠倒，类似：
-  // [1,2,3,3,2,1] => [3,2,1,3,2,1]
-  // 然后再将前部分与后部分做对比
+  let slow = head
+  let fast = head
+  let slowPrev = null
 
-  // 快慢指针
-  let slow = (fast = head)
-  // 链表上一位标识
-  let prev
-  while (fast && fast.next) {
-    fast = fast.next.next
+  let isOdd = true
 
-    // slow 开始反转
-    // 记录 slow 的下一位
-    let next = slow.next
-    // 将 slow 的上一位赋值为原上一位
-    slow.next = prev
-    // 将上一位赋值为当前一位
-    prev = slow
-    // slow 赋值为原下一位
-    slow = next
-  }
+  while (fast.next) {
+    if (fast.next && fast.next.next) {
+      fast = fast.next.next
+    } else if (fast.next && !fast.next.next) {
+      fast = fast.next
+      isOdd = false
+      break
+    }
 
-  // 如果 fast 存在，说明是该链表位数是奇数，slow 再向前走一位进入到后半段
-  if (fast) {
+    slowPrev = slow
     slow = slow.next
   }
 
-  while (prev && slow) {
-    if (prev.val !== slow.val) {
+  // 奇数则 slow 在中位，slow 的前一位才是前半段的第一位；偶数则 slow 就是前半段的第一位
+  let firstHalf = isOdd && slowPrev ? slowPrev : slow
+  let secondHalf = slow.next
+  const stopNode = firstHalf.next
+
+  let cur = head
+  let prev = null
+
+  while (cur !== stopNode) {
+    let next = cur.next
+    cur.next = prev
+    prev = cur
+    cur = next
+  }
+
+  while (!firstHalf || !secondHalf) {
+    if (firstHalf.val !== secondHalf.val) {
       return false
     }
-    prev = prev.next
-    slow = slow.next
+    if (!firstHalf.next && !firstHalf.next) {
+      return true
+    }
+    firstHalf = firstHalf.next
+    secondHalf = secondHalf.next
   }
-  return true
+
+  return false
 }
+
+// var isPalindrome = function (head) {
+//   // 本题先通过快慢指针，将数组前部分颠倒，类似：
+//   // [1,2,3,3,2,1] => [3,2,1,3,2,1]
+//   // 然后再将前部分与后部分做对比
+
+//   // 快慢指针
+//   let slow = (fast = head)
+//   // 链表上一位标识
+//   let prev
+//   while (fast && fast.next) {
+//     fast = fast.next.next
+
+//     // slow 开始反转
+//     // 记录 slow 的下一位
+//     let next = slow.next
+//     // 将 slow 的上一位赋值为原上一位
+//     slow.next = prev
+//     // 将上一位赋值为当前一位
+//     prev = slow
+//     // slow 赋值为原下一位
+//     slow = next
+//   }
+
+//   // 如果 fast 存在，说明是该链表位数是奇数，slow 再向前走一位进入到后半段
+//   if (fast) {
+//     slow = slow.next
+//   }
+
+//   while (prev && slow) {
+//     if (prev.val !== slow.val) {
+//       return false
+//     }
+//     prev = prev.next
+//     slow = slow.next
+//   }
+//   return true
+// }
 // @lc code=end
